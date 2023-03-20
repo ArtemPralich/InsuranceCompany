e31e3912b64e00788d19e8b23d29c7925a9768a4
Artem Pralich <ArtemPralich>
2023-03-20 13:02:00 +0300

    added angular material

--- InsuranceCompany.Client/src/app/app.component.ts
+++ InsuranceCompany.Client/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'InsuranceCompany.Client';
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
 }
