d4f63e500c5de0c39e6d26718a9a5c0772af8c3f
Наталья Суховило <natasha4562@yandex.com>
2023-03-22 23:07:58 +0300

    added cards

--- InsuranceCompany.Client/src/app/app.module.ts
+++ InsuranceCompany.Client/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 import { LoginComponent } from './pages/login/login.component';
 import { MainComponent } from './pages/main/main.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
