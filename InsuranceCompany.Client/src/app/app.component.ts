5864a4576fd4c7a9b8cade0c427d96d385571a41
Наталья Суховило <natasha4562@yandex.com>
2023-04-02 23:21:56 +0300

    added points and adaptation

--- InsuranceCompany.Client/src/app/app.component.ts
+++ InsuranceCompany.Client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
 
 
@@ -13,4 +13,15 @@ export class AppComponent {
   hide = true;
   title = 'InsuranceCompany.Client';
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+
+  constructor(private el: ElementRef) {}
+
+  toggleClass() {
+    const element = this.el.nativeElement.querySelector('.menu-slide');
+    if (element.classList.contains('disp')) {
+      element.classList.remove('disp');
+    } else {
+      element.classList.add('disp');
+    }
+  }
 }
